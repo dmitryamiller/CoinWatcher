c8a96b2ab875ce5a2b6a1154dba1dc15cc7ffd2d
Dmitry Miller <dmitry@cue.me>
2017-09-03 19:28:53 -0700

    Add Etherium support

---
+++ CoinWatch/CoinWatcherError.swift
@@ -0,0 +1,14 @@
+//
+//  CoinWatcherError.swift
+//  CoinWatch
+//
+//  Created by Dmitry Miller on 9/3/17.
+//  Copyright © 2017 Dmitry Miller. All rights reserved.
+//
+
+import Foundation
+
+enum CoinWatcherError: Error {
+    case badJSON
+    case unknown
+}
