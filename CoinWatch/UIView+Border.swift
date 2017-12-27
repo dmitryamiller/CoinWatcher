d3abc1a76ad04c1153d7f13b0eaedf5600989a54
Dmitry Miller <dmitry@cue.me>
2017-12-26 16:11:47 -0800

    Finish up transaction UI polish

--- CoinWatch/UIView+Border.swift
+++ CoinWatch/UIView+Border.swift
@@ -8,6 +8,23 @@
 
 import UIKit
 
-class UIView_Border: NSObject {
-
+extension UIView {
+    @IBInspectable
+    public var borderWidth: CGFloat {
+        get {
+            return self.layer.borderWidth
+        } set {
+            self.layer.borderWidth = self.borderWidth
+        }
+    }
+    
+    @IBInspectable
+    public var borderColor: UIColor? {
+        get {
+            guard let borderColor = self.layer.borderColor else { return nil }
+            return UIColor(cgColor: borderColor)
+        } set {
+            self.layer.borderColor = self.borderColor?.cgColor
+        }
+    }
 }
